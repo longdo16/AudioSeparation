33181479856da01924bd33de39c7659681448fc8
Long Do <longhoangdo16@gmail.com>
2023-11-12 23:07:37 -0600

    fix ICA

--- AudioSeparation/ICA.py
+++ AudioSeparation/ICA.py
@@ -26,17 +26,19 @@ class ICA():
         t = X.shape[1]
         m = X.shape[0]
         W = np.random.rand(n, m) / self.factor + self.epsilon
-        W = torch.from_numpy(W).to(self.device)
-        X = torch.from_numpy(X).to(self.device)
 
         # Initialize the (n by m) matrix W with small random values
         W = np.random.rand(n, m) / 10.0 + 1e-10
+
+        W = torch.from_numpy(W).to(self.device)
+        X = torch.from_numpy(X).to(self.device)
 
         for i in range(self.max_iter):
 
             temp = t // self.l
 
             sumDeltaW = np.zeros((n, m))
+            sumDeltaW = X = torch.from_numpy(sumDeltaW).to(self.device)
 
             for j in range(temp + 1):
                 data = X[:, j * 1 : (j + 1) * 1]
@@ -47,11 +49,12 @@ class ICA():
                 Z = 1.0 / (1.0 + torch.exp(-1.0 * Y))
                 I = torch.eye(n).to(self.device)
                 delta_W = self.lr * torch.matmul((I + torch.matmul((1.0 - 2.0 * Z), Y.T)), W)
-                W = W + delta_W
+                sumDeltaW += delta_W
             
             W = W + sumDeltaW
 
-        return np.matmul(W, X)
+        S = torch.matmul(W, X).numpy(force = True)
+        return S[0, :], S[1, :]
     
     def predict(self, X):
         n = X.shape[0]

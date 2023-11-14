58fcd14e6d15bbf85c08c522f945e4ffea6c321c
Long Do <longhoangdo16@gmail.com>
2023-11-13 18:53:31 -0600

    use numpy instead of pytorch tensor

--- AudioSeparation/ICA.py
+++ AudioSeparation/ICA.py
@@ -30,33 +30,20 @@ class ICA():
         for i in range(10000):
 
           temp = t // self.l
-
           sumDeltaW = np.zeros((n, m))
 
           for j in range(temp + 1):
+
             data = X[:, j * 1 : (j + 1) * 1]
-
-            # Calculate Y = WX (Y is our current estimate of the source signals)
             Y = np.matmul(W, data)
-
-            # Calculate Z
             Z = 1.0 / (1.0 + np.exp(-1 * Y))
-
-            # Find delta W
             deltaW = self.lr * np.matmul((np.identity(n) + np.matmul((1.0 - 2.0 * Z), Y.T)), W)
-
             sumDeltaW += deltaW
 
-            # deltaW = deltaW / np.linalg.norm(deltaW)
-
-            # Update W
-            # W = W + deltaW
           W = W + sumDeltaW
 
-          if i != 0 and i % 100 == 0:
-            print(np.linalg.norm(sumDeltaW))
-
         S = np.matmul(W, X)
+
         return S[0, :], S[1, :]
     
     def predict(self, X):
@@ -64,17 +51,14 @@ class ICA():
         t = X.shape[1]
         m = X.shape[0]
         W = np.random.rand(n, m) / self.factor + self.epsilon
-        W = torch.from_numpy(W).to(self.device)
-        X = torch.from_numpy(X).to(self.device)
 
         for i in range(self.max_iter):
             # Current estimate of the source signal
-            Y = torch.matmul(W, X)
+            Y = np.matmul(W, X)
 
-            Z = 1.0 / (1.0 + torch.exp(-1.0 * Y))
-            I = torch.eye(n).to(self.device)
-            delta_W = self.lr * torch.matmul((I + torch.matmul((1.0 - 2.0 * Z), Y.T)), W)
+            Z = 1.0 / (1.0 + np.exp(-1.0 * Y))
+            delta_W = self.lr * np.matmul((np.identity(n) + np.matmul((1.0 - 2.0 * Z), Y.T)), W)
             W = W + delta_W
 
-        S = torch.matmul(W, X).numpy(force = True)
+        S = np.matmul(W, X)
         return S[0, :], S[1, :]
--- AudioSeparation/NMF.py
+++ AudioSeparation/NMF.py
@@ -23,23 +23,19 @@ class NMF():
         self.hop_length = hop_length
 
     def predict(self, X):
-
         V, angle = self.process(X)
-
         K = V.shape[0]
         N = V.shape[1]
-        W = np.abs(np.random.normal(loc = 0, scale = 2.5, size = (K, self.S)))
-        H = np.abs(np.random.normal(loc = 0, scale = 2.5, size = (self.S, N)))
-        W = torch.from_numpy(W).to(self.device)
-        H = torch.from_numpy(H).to(self.device)
-        V = torch.from_numpy(V + self.epsilon)
+        W = np.abs(np.random.normal(loc=0, scale = 2.5, size=(K,S)))
+        H = np.abs(np.random.normal(loc=0, scale = 2.5, size=(S,N)))
 
         for i in range(self.max_iter):
-            H = (torch.mul(H, (torch.matmul(W.T, torch.mul(torch.matmul(W, H)**(self.beta - 2), V))) / (torch.matmul(W.T, torch.matmul(W, H)**(self.beta - 1))+ self.epsilon)))
-            W = (torch.mul(W, (torch.matmul(torch.mul(torch.matmul(W, H)**(self.beta - 2), V), H.T)) / (torch.matmul(torch.matmul(W, H)**(self.beta - 1), H.T)+ self.epsilon)))
+            
+            H = (np.multiply(H, (np.matmul(W.T, np.multiply(np.matmul(W, H)**(self.beta - 2), V))) / (np.matmul(W.T, np.matmul(W, H)**(self.beta - 1))+ 10e-10)))
+            W = (np.multiply(W, (np.matmul(np.multiply(np.matmul(W, H)**(self.beta - 2), V), H.T)) / (np.matmul(np.matmul(W, H)**(self.beta - 1), H.T)+ 10e-10)))
         
-        S1 = self.separate_source(0, W, H, angle).numpy(force = True)
-        S2 = self.separate_source(1, W, H, angle).numpy(force = True)
+        S1 = self.separate_source(0, W, H, angle)
+        S2 = self.separate_source(1, W, H, angle)
 
         return S1, S2
         
@@ -52,8 +48,8 @@ class NMF():
         return V, X_stft_angle
     
     def separate_source(self, index, W, H, angle):
-        filtered_spectrograms = torch.matmul(W[:, [index]], H[[index], :])
-        reconstructed_amp = filtered_spectrograms * torch.exp(1j*angle)
-        reconstructed_audio = torchaudio.transforms.InverseSpectrogram(n_fft = self.n_fft)(reconstructed_amp)
+        filtered_spectrograms = np.matmul(W[:,[index]], H[[index],:])
+        reconstructed_amp = filtered_spectrograms[index] * np.exp(1j * angle)
+        reconstructed_audio = librosa.istft(reconstructed_amp, n_fft = self.n_fft, hop_length = self.hop_length)
         
         return reconstructed_audio
